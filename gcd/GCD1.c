e2acefae630784117c01cd9a8e260a6cff2663a7
tatatatam <tam_onjoy@hotmail.com>
2017-08-26 21:00:39 -0700

    Add files via upload

--- gcd/GCD1.c
+++ gcd/GCD1.c
@@ -1,49 +1,71 @@
 #include<stdio.h>
-  int gcd(int m,int n);
-  int main(){
-    int c=1;
-    FILE *fp;
-    char buff[255];
-    int i =0;
-    int j =0;
+#include <stdlib.h>
+// #include<limits.h>
+  long long gcd(long long m,long long n);
+  long long main(){
+    printf("%lli\n",gcd(111, 234));
+    printf("%lli\n",gcd(60, 225) );
+    printf("%lli\n",gcd(42, 584010));
+        printf("%lli\n",gcd(11, 13));
+    printf("%lli\n",gcd(7854, 4746));
+    printf("%lli\n",gcd(37894060279, 18272779829));
+    printf("%lli\n",gcd(742271756, 606228865));
+    printf("%lli\n",gcd(0, -1));
+    printf("%lli\n",gcd(gcd(-15, 0) ,gcd(20, -25)));
+    printf("%lli\n",gcd(gcd(36, 84) ,120));
+    printf("%lli\n",gcd(gcd(189, 252), gcd(1197, 292005)));
+    printf("%lli\n",gcd(0,1));
+    printf("%lli\n",gcd(gcd(-15, 0) ,gcd(20, -25)));
 
+    // int c=1;
+    FILE *fp;
+    long long i =0;
+    long long j =0;
+    printf("fileGCD1\n");
     fp = fopen("testcase.txt","r");
-    fscanf (fp, "%d", &i);
+    fscanf (fp, "%lli", &i);
     while (!feof (fp))
       {
-        // printf ("%d AND %d ", i,j);
-        fscanf (fp, "%d", &i);
-        fscanf (fp, "%d", &j);
-        printf("%d and %d GCD ASWER: %d\n",i,j, gcd(i,j));
+        // printf ("%lli AND %lli ", i,j);
+        fscanf (fp, "%lli", &i);
+        fscanf (fp, "%lli", &j);
+        printf("%lli and %lli GCD ASWER: %lli\n",i,j, gcd(i,j));
       }
        fclose(fp);
     return 0;
   }
 
-  int gcd(int m,int n){
-    int t ;
-    int bool = 1;
-    if(m<n){
-      t=m;
-    }else if(m>n){
-      t=n;
-    }else{
-      bool = 0;
-      return t;
-
+  long long gcd(long long m,long long n){
+    long long t ;
+    long long bool = 1;
+    long long count=0;
+    if(m<n){//n-1
+      t=m;//n-1
+    }else{//n-1
+      t=n;//n-1
     }
-
-    while(bool==1){
-      // printf("%d\n", t);
-      if( m%t== 0 ){
-        if(n%t==0){
-          bool = 0;
-          return t;
-        }else{
-          t--;
-          }
-        }else{
-          t--;
-        }
+    m= abs(m);//n
+    n= abs(n);//n
+    t= abs(t);//n
+    while(bool==1){//n
+      if(m==0||n==0){//n-1
+      bool = 0;//n-1
+      return 0;//n-1
       }
+      else if(m%t== 0 ){//n-1
+        count++;//n-1
+        if(n%t==0){//2n-1
+          count++;//2n-1
+          bool = 0;//2n-1
+          printf("counter:%lli\n ",count);
+          return t;
+        }else{//2n-1
+          count++;//2n-1
+           t--;//2n-1
+          }
+      }else{//n-1
+         count++;//n-1
+         t--;//n-1
+        }
     }
+  }
--- gcd/GCD2.c
+++ gcd/GCD2.c
@@ -1,11 +1,25 @@
 #include<stdio.h>
-  int gcd(int m,int n);
-  int main(){
-    int c=1;
+#include <stdlib.h>
+  long long gcd(long long m,long long n);
+  long long main(){
+    long long c=1;
     FILE *fp;
     char buff[255];
-    int i =0;
-    int j =0;
+    long long i =0;
+    long long j =0;
+    printf("%lli\n",gcd(111, 234));
+    printf("%lli\n",gcd(60, 225) );
+    printf("%lli\n",gcd(42, 584010));
+    printf("%lli\n",gcd(11, 13));
+    printf("%lli\n",gcd(7854, 4746));
+    printf("%lli\n",gcd(37894060279, 18272779829));
+    printf("%lli\n",gcd(742271756, 606228865));
+    printf("%lli\n",gcd(0, -1));
+    printf("%lli\n",gcd(gcd(-15, 0) ,gcd(20, -25)));
+    printf("%lli\n",gcd(gcd(36, 84) ,120));
+    printf("%lli\n",gcd(gcd(189, 252), gcd(1197, 292005)));
+    printf("%lli\n",gcd(0,1));
+    printf("%lli\n",gcd(gcd(-15, 0) ,gcd(20, -25)));
 
     fp = fopen("testcase.txt","r");
     fscanf (fp, "%d", &i);
@@ -16,32 +30,44 @@
         fscanf (fp, "%d", &j);
         printf("%d and %d GCD ASWER: %d\n",i,j, gcd(i,j));
       }
-       fclose(fp);
+    //    fclose(fp);
+
        return 0;
   }
-  int gcd(int m , int n){
-    int i=m;
-    int j=n;
-    int modder = 2;
-    int res =1;
-    int bool = 1;
+  long long gcd(long long m , long long n){
+    long long i=abs(m); //n
+    long long j=abs(n);//n
+    long long modder = 2;
+    long long res =1;
+    long long count =0;
+    m=abs(m);//n
+    n=abs(n);//n
 
-    while((m!=1&&n!=1)){
-      if(i%modder==0&&j%modder==0){
-        while(i%modder==0&&j%modder==0){
-          i = i/modder;
-          j = j/modder;
-          // printf("%d------%d---%d\n",i,j,modder);
-          res*=modder;
+    while((m>1&&n>1)){ //n
+
+    if(i%modder==0&&j%modder==0){//n-1;
+        count++;//n-1
+        while(i%modder==0&&j%modder==0){//2n-1
+          count++;//2n-1
+          i = i/modder;//2n-1
+          j = j/modder;//2n-1
+          res*=modder;//2n-1
+          // printf("I:%d J:%d res:%d\n",i,j,res);
         }
-        // printf("%d",res);
-      }else if(m==modder||n==modder){
-        m = m/modder;
-        m = n/modder;
-        // return m;
+
+      }else if(m==modder||n==modder){//n-1
+        count++;//n-1
+        m = m/modder;//n-1
+        n = n/modder;//n-1
+
+      }else{//n-1
+        count++;//n-1
       }
-      // prisntf("modder%d\n%",m);
-        modder++;
+        modder++;//n
     }
+    if(m==0||n==0){//n-1
+      res =0;//n-1
+    }
+    printf("Count: %d\n",count );
     return res;
   }
